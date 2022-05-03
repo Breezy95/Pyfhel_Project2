83dd19d0459f45a707237c1c8976f474bfcdf5a0
Breezy95 <fabrivlad1995@gmail.com>
2022-05-03 18:58:19 -0400

    created simpler function for sending data to fake server

--- clientSide.py
+++ clientSide.py
@@ -1,71 +1,85 @@
-"""
-Client/Server demo with Pyfhel
-========================================
-
-Context Parameters shows how several parameters affect performance.
-"""
-
 from Pyfhel import Pyfhel, PyPtxt, PyCtxt
-import tempfile
-import json
-from pathlib import Path
+import os
 import socket
-import numpy as np
-import csv
 import pickle
+import struct
 
-# Using a temporary dir as a "secure channel"
-# This can be changed into real communication using other python libraries.
-secure_channel = tempfile.TemporaryDirectory()
-sec_con = Path(secure_channel.name)
+#process which iswhat we want sent(pk, ctxt,context), filename, socket
+def sendFile(proc,fn,sock):
+    s.send(bytes(proc))  #send process to be done
+    v = s.recv(1024).decode()   #wait for ack of prev msg
+    print(f"received process ack: {v}")
+#send file name
+
+    s.send(fn.encode())
+
+    v=s.recv(1024).decode()
+    print(f"received filename ack : {v}")
+#send file size
+    print(os.path.getsize(fn))
+#use struct pack to send file size (is it needed?)
+    s.send(struct.pack("i", os.path.getsize(fn)))
+    v =s.recv(1024).decode() #wait for ack
+    print("received  file size ack: {v}")
+
+
+#send file contents
+#send in segments so theres no weird file overlap with
+#other files
+    with open(fn, 'rb') as file_contents:
+        fc = file_contents.read(1024)
+        while fc:
+            s.send(fc)
+            print("sending segment")
+        #print(str(fc))
+            fc = file_contents.read(1024)
+
+
+
+
+HE = Pyfhel()
+HE.contextGen(p=65537, m=2**12)
+HE.keyGen()
+
+print(HE)
+
 pk_file = "mypk.pk"
-contx_file = "mycontx.con"
-
-
-##### CLIENT
-#HE Object Creation, including the public and private keys
-HE = Pyfhel()    
-HE.contextGen(p=65537, m=2**12) 
-HE.keyGen() # Generates both a public and a private key
-
-# Saving only the public key and the context
 HE.savepublicKey(pk_file)
-HE.saveContext(contx_file)
 
-#for the sake of brevity we're going to send a list of values and then the server will
-# for this example send back one Ctxt object with the operation done on it
-csvDict = {}
-with open("toy_dataset.csv", "r", newline='') as f:
-    csvReader = csv.DictReader(f)
-    for entry in csvReader:
-        csvDict[entry['Number']] = [entry['City'],entry['Gender'],entry['Age'], entry['Illness']]  #city,gender,age,income,illness
+ctx_file = "myctx.con"
+HE.saveContext(ctx_file)
+a = 1.5
+b = 2.5
+ca = HE.encryptFrac(a)
+cb = HE.encryptFrac(b)
+ca.to_file('ca.ctxt')
+cb.to_file('cb.ctxt')
 
-#serialize the public key, context and send over the network
+#send files to server
+HOST = '127.0.0.1'
+PORT = 50001
+
+#serialize pub key
+pick_pk = pickle.dumps(HE)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+#pk or ctxt
+sendFile('pk',pk_file,s)
+
+            
 
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-
-    for x in range(1,11): #1 to 10 0 is not in the csv as a starting point
-        val =int(csvDict[str(x)][2])
-        encVal =HE.encryptInt(val)
 
 
 
-'''
-#encrypt values and then send each value over to network
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    for x in range(1,11): #1 to 10 0 is not in the csv as a starting point
-        val =int(csvDict[str(x)][2])
-        HE.encryptInt(val)
-        #pickle value and send over connection
-        print(f"Received {data!r}")
-'''
+
+
+
+
+
+
+
--- serverSide.py
+++ serverSide.py
@@ -1,34 +1,51 @@
 from Pyfhel import Pyfhel, PyPtxt, PyCtxt
-import tempfile
-import json
-from pathlib import Path
-import socketserver
-import pickle
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+import os
+import sys
 import socket
+import pickle
+import struct
+import regex
+
+#server acts as FTP server, then performs computation.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 50001))
+s.listen(1)
+conn, addr = s.accept()
+BUFFER = 1024
+data = conn.recv(BUFFER).decode()
+if data == 'pk':
+    conn.send(b'1') #ack of method
+
+    #retrieve file name
+    file_header = struct.unpack("h", conn.recv(2))[0]
+    filename = conn.recv(file_header).decode()
+
+    conn.send(b'1') #ack of file name
+
+    #retrieve file size
+    file_size = struct.unpack("i", conn.recv(4))[0]
+
+    conn.send(b'1')
+
+    #loop for file contents
+    pk_file =open("server_file/" +str(filename), 'wb')
+    bytes =0
+    while bytes < file_size:
+        file_buffer = conn.recv(BUFFER)
+        pk_file.write(file_buffer)
+        bytes += len(file_buffer)
+    pk_file.close()
+
+
+    
+
+
+#upon receiving files it will create the public key and then perform calculations
 
 
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432
-x = PyCtxt()
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        HE_CL = Pyfhel()
-
-'''
-
-class MyHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        addr = self.request.
-        print a    
-
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-x = []
-
-
-'''
-
+#might be better to use some webserver functionality here
+#that way it can receive and do the math without adding complex code
