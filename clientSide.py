895b310c93429c4ee947285af8583c0bfd125143
Breezy95 <fabrivlad1995@gmail.com>
2022-05-04 02:02:55 -0400

    adding multiple options

--- clientSide.py
+++ clientSide.py
@@ -10,6 +10,7 @@ import struct
 #should receive a ciphertext
 def reqData(sock, operation):
     sock.send(bytes(operation))
+
     with open(operation, 'wb') as f:
         bytes =0
         
@@ -20,18 +21,20 @@ def sendFile(proc,fn,sock):
     v = sock.recv(1024).decode()   #wait for ack of prev msg
     print(f"received process ack: {v}")
 #send file name
-
+    print(fn)
     sock.send(fn.encode())
 
     v=sock.recv(1024).decode()
     print(f"received filename ack : {v}")
+
+    #pickle file here
+    #pickled_file = pickle.dumps()
 #send file size
     print(os.path.getsize(fn))
 #use struct pack to send file size (is it needed?)
     sock.send(struct.pack("i", os.path.getsize(fn)))
     v =sock.recv(1024).decode() #wait for ack
     print("received  file size ack: {v}")
-
 
 #send file contents
 #send in segments so theres no weird file overlap with
@@ -46,42 +49,49 @@ def sendFile(proc,fn,sock):
 
 
 
+if __name__ == "__main__":
+    
+    HE = Pyfhel()
+    HE.contextGen(p=65537, m=2**12)
+    HE.keyGen()
 
-HE = Pyfhel()
-HE.contextGen(p=65537, m=2**12)
-HE.keyGen()
+    print(HE)
 
-print(HE)
+    #pk_file = "mypk.pk"
+    #HE.savepublicKey(pk_file)
 
-#pk_file = "mypk.pk"
-#HE.savepublicKey(pk_file)
+    ctx_file = "myctx.con"
+    HE.saveContext(ctx_file)
+    a = 1.5
+    b = 2.5
+    ca = HE.encryptFrac(a)
+    cb = HE.encryptFrac(b)
+    ca.to_file('ca.ctxt')
+    cb.to_file('cb.ctxt')
 
-ctx_file = "myctx.con"
-HE.saveContext(ctx_file)
-a = 1.5
-b = 2.5
-ca = HE.encryptFrac(a)
-cb = HE.encryptFrac(b)
-ca.to_file('ca.ctxt')
-cb.to_file('cb.ctxt')
+    #send files to server
+    HOST = '127.0.0.1'
+    PORT = 50001
 
-#send files to server
-HOST = '127.0.0.1'
-PORT = 50001
-
-#serialize pub key
-pick_pk = pickle.dumps(HE)
-with open("pickled_pk.pk","wb") as pk_f:
-    pk_f.write(pick_pk)
+    #serialize pub key
+    pick_pk = pickle.dumps(HE)
+    with open("pick_pk.pk","wb") as pk_f:
+        pk_f.write(pick_pk)
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST,PORT))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
 
-#pk or ctxt
-sendFile('pk','pickled_pk.pk',s)
+    #pk,HE,ciphtxt,ctx
+    sendFile('pk','pick_pk.pk',s)
+    print("exit method")
+    sendFile('pk','pick_pk.pk',s)
+    
+    #s.send(bytes('ctxt','utf8'))
+    #sendFile('HE','ca.ctxt',s)
 
- 
+
+    
 
 
 
--- serverSide.py
+++ serverSide.py
@@ -9,57 +9,140 @@ import pickle
 import struct
 import regex
 
-#server acts as FTP server, then performs computation.
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 50001))
-s.listen(1)
-conn, addr = s.accept()
 BUFFER = 1024
-data = conn.recv(BUFFER).decode()
-if data == 'pk':
-    conn.send(b'1') #ack of method
+
+#server acts as FTP server, then performs computation.
+def pkSetup(sock):
+    sock.send(b'1') #ack of method
 
     #retrieve file name
-    file_header = struct.unpack("h", conn.recv(2))[0]
-    filename = conn.recv(file_header).decode()
+    file_header = struct.unpack("h", sock.recv(2))[0]
+    filename = sock.recv(file_header).decode()
 
-    conn.send(b'1') #ack of file name
+    sock.send(b'1') #ack of file name
 
     #retrieve file size
-    file_size = struct.unpack("i", conn.recv(4))[0]
+    file_size = struct.unpack("i", sock.recv(4))[0]
 
-    conn.send(b'1')
+    sock.send(b'1')
 
-    #loop for file contents
+    #receive pickled file
+    #contents are consistently small enough that 
+    # I decided to leave this as one line
+    pick_f =sock.recv(1024)
+    print()
+    #returns a byte stream
+    return pick_f
+
+
+
+'''
+#context is pickled inside HE
+def HESetup(sock):
+    sock.send(b'1') #ack of method
+
+    #retrieve file name
+    file_header = struct.unpack("h", sock.recv(2))[0]
+    filename = sock.recv(file_header).decode()
+
+    sock.send(b'1') #ack of file name
+
+    #retrieve file size
+    file_size = struct.unpack("i", sock.recv(4))[0]
+
+    sock.send(b'1')
+
+def addCtxt(sock):
+    sock.sendall(bytes('1','utf8')) #ack of method
+
+    #retrieve file name
+    file_header = struct.unpack("h", sock.recv(2))[0]
+    filename = sock.recv(file_header).decode()
+
+    sock.send(b'1') #ack of file name
+
+    #retrieve file size
+    file_size = struct.unpack("i", sock.recv(4))[0]
+
+    sock.send(b'1')
+
+    #receive pickled file stream and write
+
     pk_file =open("server_file/" +str(filename), 'wb')
     bytes =0
     while bytes < file_size:
-        file_buffer = conn.recv(BUFFER)
+        file_buffer = sock.recv(BUFFER)
         pk_file.write(file_buffer)
         bytes += len(file_buffer)
     
     pk_file.close()
+    unpick_f =pickle.load(pk_file)
+    return unpick_f
+    
+'''
+
 
 
 #main method, switch statement:
 # global vars: HE, use regex to load list of 
 # ciphertexts
 if __name__ == "__main__":    
-    HE = object()
+    HE_CL = Pyfhel()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('localhost', 50001))
     s.listen(1)
     conn, addr = s.accept()
     val = conn.recv(1024).decode()
+    #when pickling a pyfhel object only context is serialized
+    #keys must be transmitted independently
+    with conn:
+        while True:    
+            if val == 'pk':
+              x =pkSetup(conn)
+              print(x)
+              HE_pk =pickle.loads(x)
+              #HE_CL.from_bytes_publicKey(HE_pk)
+              conn.send(b'1') 
+              val = conn.recv(1024).decode()
+            #might be unnecessary
+            elif val == 'HE':
+             # x = HESetup(s)
+             # HE_CL = x
+              val = conn.recv(1024).decode()
+
+            elif val == 'ctxt':
+              #  addCtxt(s)
+                val = s.recv(1024).decode()
+
+            else:
+                print("exiting program")
+
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+    '''
+    need python3.10 for match statements
     match val:
         case 'pk':
             pass
         case 'ctxt':
             pass
+        case 'add':
+            pass
         case _:
-            conn.send(b'invalid query, ending session and deleting files')
-            print('invalid query, ending session and deleting files')
-
+            conn.send(b'invalid query')
+            print('invalid query')
+'''
 #upon receiving files it will create the public key and then perform calculations
 
 
